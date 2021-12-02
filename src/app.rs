a84fcd62576545417fbdbf4f1dec8a2f58ec540f
Luke Patrick <luke@routerchowder.com>
2021-12-01 21:07:17 -0700

    feat: add is_chain_valid and choose_chain methods to App implement

--- src/app.rs
+++ src/app.rs
@@ -55,6 +55,38 @@ impl App {
         }
         true
     }
+    fn is_chain_valid(&self, chain: &[Block]) -> bool {
+        for i in 0..chain.len() {
+            if i == 0 {
+                continue;
+            }
+            let first = chain.get(i - 1).expect("has to exist");
+            let second = chain.get(i).expect("has to exist");
+            if !self.is_block_valid(second, first) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn choose_chain(&mut self, local: Vec<Block>, remote: Vec<Block>) -> Vec<Block> {
+        let is_local_valid = self.is_chain_valid(&local);
+        let is_remote_valid = self.is_chain_valid(&remote);
+
+        if is_local_valid && is_remote_valid {
+            if local.len() >= remote.len() {
+                local
+            } else {
+                remote
+            }
+        } else if is_remote_valid && !is_local_valid {
+            remote
+        } else if !is_remote_valid && is_local_valid {
+            local
+        } else {
+            panic!("local and remote chains are both invalid");
+        }
+    }
 
     fn try_add_block(&mut self, block: Block) {
         let latest_block = self.blocks.last().expect("there is at least one block");
